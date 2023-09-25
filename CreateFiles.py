a2eb76b9bf40fd5209d34ab74258dd236600ad40
LocalT0aster <localt0aster@proton.me>
2023-09-26 00:06:14 +0300

    CreateFiles script

---
+++ CreateFiles.py
@@ -0,0 +1,29 @@
+import os
+import stat
+
+week = 3
+basepath = f'week{str(0) + str(week) if week < 10 else str(week)}/'
+try:
+    os.mkdir(basepath)
+except FileExistsError:
+    print('Folder already exists!')
+
+for i in range(1,7):
+    try:
+        with open(f'{basepath}ex{i}.c', 'x') as f:
+            f.write(open('week01/main.c').read())
+            f.flush()
+            f.close()
+    except FileExistsError:
+        print('File exists!')
+    try:
+        shname = f'{basepath}ex{i}.sh'
+        with open(shname, 'x')as f:
+            f.write(f'#!/bin/bash\ngcc -O3 ex{i}.c -o ex{i} -lm\n./ex{i}\nrm ex{i}\n')
+            f.flush()
+            f.close()
+        os.chmod(shname, stat.S_IMODE(os.stat(shname).st_mode | stat.S_IEXEC))
+    except FileExistsError:
+        print('File exists!')
+
+print('Done')
