c306d312ca1691a093bb20a6769597abf6f19a27
LocalT0aster <localt0aster@proton.me>
2023-09-27 00:02:49 +0300

    Lab 3 ex6

--- week03/ex6.c
+++ week03/ex6.c
@@ -7,6 +7,31 @@ void swap(T* a, T* b) {
     *b = temp;
 }
 
+int partition(T* arr, int low, int high) {
+    T pivot = arr[high];
+    int i = (low - 1); // Index of the smaller element
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++; // Increment index of the smaller element
+            swap(&arr[i], &arr[j]);
+        }
+    }
+    swap(&arr[i + 1], &arr[high]);
+    return (i + 1);
+}
+
+void quicksort_recursive(T* arr, int low, int high) {
+    if (low < high) {
+        // pi is partitioning index, arr[pi] is now at the right place
+        int pi = partition(arr, low, high);
+
+        // Separately sort elements before and after partition
+        quicksort_recursive(arr, low, pi - 1);
+        quicksort_recursive(arr, pi + 1, high);
+    }
+}
+
 void quicksort(T* arr, size_t size) {
     if (size <= 1) return;
     if (size == 2) {
@@ -15,13 +40,16 @@ void quicksort(T* arr, size_t size) {
             return;
         }
     }
-    T piv = size>>1; // Place pivot in the middle.
-    int l = piv - 1;
-    int r = piv;
-    piv = arr[piv];
+    quicksort_recursive(arr, 0, size - 1);
 }
 
 int main() {
-    
+    T arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    quicksort(arr, n);
+    printf("Sorted array: \n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
     return 0;
 }
