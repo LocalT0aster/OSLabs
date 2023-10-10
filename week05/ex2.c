02a1ebf9c6bde25126e921565e13b5be73182114
LocalT0aster <localt0aster@proton.me>
2023-10-10 21:53:53 +0300

    week05 ex2

--- week05/ex2.c
+++ week05/ex2.c
@@ -1,5 +1,49 @@
-#include<stdio.h>
-int main() {
-    printf("Hello World!\n");
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+
+typedef struct {
+    pthread_t id;
+    int i;
+    char message[256];
+} Thread;
+
+// Thread function
+void *print_message(void *ptr) {
+    Thread *thread_data = (Thread *)ptr;
+    printf("Thread %lu: %s\n", thread_data->id, thread_data->message);
+    printf("Thread %d exits\n", thread_data->i + 1);
+    pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("Usage: ./ex2.exe <number of threads>\n");
+        exit(1);
+    }
+
+    int n = atoi(argv[1]);
+    Thread threads[n];
+
+    for (int i = 0; i < n; i++) {
+        threads[i].i = i;
+        sprintf(threads[i].message, "Hello from thread %d", i + 1);
+
+        int ret = pthread_create(&threads[i].id, NULL, print_message, &threads[i]);
+
+        if (ret) {
+            printf("Error creating thread. Error code: %d\n", ret);
+            exit(-1);
+        }
+
+        printf("Thread %d is created\n", i + 1);
+        
+        // Ensure messages are printed in order
+        pthread_join(threads[i].id, NULL);
+    }
+
     return 0;
 }
--- week05/ex2.sh
+++ week05/ex2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-gcc ex2.c -o ex2.exe -lm -Wextra -Wpedantic
-./ex2.exe
+if [ "$#" -ne 1 ]; then
+    echo "Usage: ./ex2.sh <number of threads>"
+    exit 1
+fi
+gcc ex2.c -o ex2.exe -pthread -lm -Wextra -Wpedantic
+./ex2.exe $1
 rm ex2.exe
