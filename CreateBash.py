f4be68cec889ff0d712c0b8a18ea6bdbd374eecc
LocalT0aster <localt0aster@proton.me>
2023-10-17 17:12:58 +0300

    CreateBash task

--- CreateBash.py
+++ CreateBash.py
@@ -1,21 +1,17 @@
 import os
 import stat
 
-week = 6
+print("Enter the number of week:")
+week = int(input())
+print("Enter number of shell scripts to create:")
+n = int(input()) + 1
 basepath = f'week{str(0) + str(week) if week < 10 else str(week)}/'
 try:
     os.mkdir(basepath)
 except FileExistsError:
     print('Folder already exists!')
 
-for i in range(1,7):
-    # try:
-    #     with open(f'{basepath}ex{i}.c', 'x') as f:
-    #         f.write(open('week01/main.c').read())
-    #         f.flush()
-    #         f.close()
-    # except FileExistsError:
-    #     print('File exists!')
+for i in range(1,n):
     try:
         shname = f'{basepath}ex{i}.sh'
         with open(shname, 'x')as f:
