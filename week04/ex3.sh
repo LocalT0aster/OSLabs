c74255712505855b24fff7ecbda8ee4d896e9b27
LocalT0aster <90502400+LocalT0aster@users.noreply.github.com>
2024-09-24 16:56:14 +0300

    week 4 ex3.sh fix

--- week04/ex3.sh
+++ week04/ex3.sh
@@ -4,7 +4,7 @@
 gcc ex3.c -o ex3.out
 
 # Run the program with n=3 in the background
-./ex3 3 &
+./ex3.out 3 &
 sleep 11
 echo "---- PSTREE OUTPUT FOR n=3 ----"
 pstree | grep ex3
@@ -13,7 +13,7 @@ pstree | grep ex3
 sleep 5
 
 # Run the program with n=5 in the background
-./ex3 5 &
+./ex3.out 5 &
 sleep 21
 echo "---- PSTREE OUTPUT FOR n=5 ----"
 pstree | grep ex3
